a91e5e3e4379e6ea8038e3320f3e9c38eef2ab8c
ErickYar <erickyb1985@gmail.com>
2023-10-29 18:30:37 -0500

    proyecto de react inicio

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// const contenido = (
+// <div>
+//   <h1> Titulo</h1>
+// <div>hola erick</div>
+// </div>
+//   );
+function Componente(){
+  return(
+    <div>
+  <h1> Titulo</h1>
+<div>hola Componente erick</div>
+</div>
+  );
+}
+ReactDOM.render(<Componente/>,document.getElementById('root'));
+
+ 
+
+
+
